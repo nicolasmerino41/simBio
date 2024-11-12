dfe79c1f514d70da4a72b5f897f4cc6a93008ad7
Nico Merino <nicolasmerino41@gmail.com>
2024-11-12 17:34:25 +0100

    Lil changes to HerpsVsBirmmals

--- Daily JULIA code/HerpsVsBirmmals.jl
+++ Daily JULIA code/HerpsVsBirmmals.jl
@@ -9,7 +9,9 @@ using Rasters, RasterDataSources #, DimensionalData
 using DynamicGrids, Dispersal
 using Dates, Distributions, Serialization, StatsBase, JLD2
 using ColorSchemes, Colors #Crayons, 
-using Makie, WGLMakie # ImageMagick, 
+using Makie, WGLMakie # ImageMagick
+using EcologicalNetworksDynamics, DifferentialEquations
+using Plots
 const DG, MK, AG, RS, Disp, DF, PL = DynamicGrids, Makie, ArchGDAL, Rasters, Dispersal, DataFrames, Plots
 const COLORMAPS = [:magma, :viridis, :cividis, :inferno, :delta, :seaborn_icefire_gradient, :seaborn_rocket_gradient, :hot]
 ######################## COMPLEX RULES #############################
