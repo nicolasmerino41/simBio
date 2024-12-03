47cf01ccf2c6e4a32386acccef3cdd34129f7a09
nicomerino <nicolasmerino41@gmail.com>
2024-12-03 19:55:02 +0100

    Lil fixes

--- Daily_JULIA_code/HerbivoresVsPredators/Plots.jl
+++ Daily_JULIA_code/HerbivoresVsPredators/Plots.jl
@@ -23,12 +23,13 @@ begin
 
     # ax1.xticks = (bar_positions, sorted_predators)
     Legend(fig1[1, 2], ax1)
-    fig1
+    display(fig1)
 end
 
 ################## Scatterplot ###########################
 # Proportion of herbivores in the diet
 proportion_herbivores = [herbivores_eaten[i] / total_prey[i] for i in 1:length(predators)]
+
 begin
     
     fig2 = Figure(resolution=(800, 400))
@@ -39,18 +40,10 @@ begin
     scatter!(ax2, total_prey, proportion_herbivores, 
              markersize=0.2 .* predators_eaten, color=:green, label="Predators")
 
-    # # Add text labels
-    # for (i, predator) in enumerate(predators)
-    #     text!(ax2, predator, position=(total_prey[i], proportion_herbivores[i] + 0.05), align=:center, fontsize=10)
-    # end
-
-    fig2
+    display(fig2)
 end
 
 ################# Heatmap for Prey Availability ########################
-# Identify species that are eaten
-filtered_spain_names = spain_names
-# Plot the filtered heatmap
 cut = true
 begin 
     fig3 = Figure(resolution=(800, 400))
@@ -62,12 +55,8 @@ begin
     end
     # Create heatmap with filtered matrix
     heatmap!(ax3, ibe', colormap=:viridis, interpolate=false)
-    
-    # Update ticks with filtered species names
-    # ax3.xticks = (1:length(filtered_spain_names), filtered_spain_names)
-    # ax3.yticks = (1:length(predators), predators)
         
-    fig3
+    display(fig3)
 end
 
 println("Corvus corone is in position: ", findfirst(x -> x == "Corvus corone", spain_names))
@@ -83,11 +72,7 @@ begin
     ax4 = Axis(fig4[1, 1], title="Predator Overlap (Shared Prey Species)", xlabel="Predators", ylabel="Predators")
     heatmap!(ax4, overlap_matrix, colormap=:blues, colorrange=(0, maximum(overlap_matrix)))
 
-    # Update ticks with predator names
-    ax4.xticks = (1:length(predators), predators)
-    ax4.yticks = (1:length(predators), predators)
-
-    fig4
+    display(fig4)
 end
 
 # Compute relative overlap matrix
@@ -98,16 +83,12 @@ relative_overlap_matrix = [
 
 # Plot relative predator overlap heatmap
 begin
-    fig4 = Figure(resolution=(800, 400))
+    fig5 = Figure(resolution=(800, 400))
     ax4 = Axis(fig4[1, 1], title="Relative Predator Overlap (Shared Prey Species)", 
                xlabel="Predators", ylabel="Predators")
     
     # Plot the relative overlap heatmap
     heatmap!(ax4, relative_overlap_matrix, colormap=:blues, colorrange=(0, 1))
 
-    # Update ticks with predator names
-    # ax4.xticks = (1:length(predators), predators)
-    # ax4.yticks = (1:length(predators), predators)
-
-    fig4
+    display(fig5)
 end
