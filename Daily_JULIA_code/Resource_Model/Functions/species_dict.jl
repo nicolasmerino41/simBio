ec5838f4e9b3954c5a8a04894f13e32788265a6c
Nico Merino <nicolasmerino41@gmail.com>
2025-01-07 12:56:22 +0100

    Added species_dict_herbivores_in_birmmals

--- Daily_JULIA_code/Resource_Model/Functions/species_dict.jl
+++ Daily_JULIA_code/Resource_Model/Functions/species_dict.jl
@@ -14,3 +14,13 @@ species_dict = Dict{String, Int}()
 for (idx, sp) in enumerate(row_species)
     species_dict[sp] = idx
 end
+
+# SAME but only to identify the herbivores positions in a MyBirmmals struct
+birmmals_names = spain_names[50:256]
+species_dict_herbivores_in_birmmals = Dict{String, Int}()
+
+for (idx, sp) in enumerate(birmmals_names)
+    if sp in herbivore_names
+        species_dict_herbivores_in_birmmals[sp] = idx
+    end
+end
