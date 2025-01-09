537bd1c0958ebbcb45ba36b3d6676f879f94c1a4
Nico Merino <nicolasmerino41@gmail.com>
2025-01-09 14:45:27 +0100

    Improve plotting SAD from output

--- Daily_JULIA_code/Resource_Model/RealModel.jl
+++ Daily_JULIA_code/Resource_Model/RealModel.jl
@@ -9,13 +9,14 @@ include("extract_H0_DA.jl")
 # We apply the dynamics using the species, variables and parameters from a chosen cell.
 ##############################################################################################
 total_biomass_df = DataFrame(herbivore_biomass = Float64[], predator_biomass = Float64[], total_biomass = Float64[], i_star = Float64[])
-i_star_idx = 4 
+
+# for i_star_idx in 1:10 
 begin
 
     ## NOTE, COMMENT THE include(callbacks.jl) IF YOU'VE ALREADY RUN IT ONCE FOR THAT CELL
-    extinction_trigger = false
+    extinction_trigger = true
     # Choose a cell
-    aco = 2500
+    aco = 417
     i, j = idx[aco][1], idx[aco][2]
 
     # Extract the NPP from the cell
@@ -23,21 +24,21 @@ begin
 
     H0_vector = Vector{Float64}(H0_DA[i, j].a)
     
-    S, R, species_names, herbivore_list, predator_list, H_i0, m_i, p_vec, x_final, g_i, G, M_modified, a_matrix, A, epsilon, m_alpha = setup_community_from_cell(
+    S, R, species_names, herbivore_list, predator_list, H_i0, m_i, p_vec, x_final, g_i, localHatH, G, M_modified, a_matrix, A, epsilon, m_alpha = setup_community_from_cell(
         i, j;
         NPP = NPP,
         M_mean = 0.1,
         mu = 0.5,
-        symmetrical_competition = false,
+        symmetrical_competition = true,
         mean_m_alpha = 0.1,
-        epsilon_val = 0.01,
+        epsilon_val = 1.0,
         mu_predation = 0.01,
         iberian_interact_NA=iberian_interact_NA,
         species_dict=species_dict,
         m_standard_deviation = 0.0,
         h_standard_deviation = 0.0,
         artificial_pi = false,
-        real_H0 = false,
+        real_H0 = true,
         H0_vector = H0_vector
     )
 
@@ -49,7 +50,7 @@ begin
     u0 = vcat(H_init, P_init)
     tspan = (0.0, 500.0)
     EXTINCTION_THRESHOLD = 1e-6
-    T_ext = 100.0
+    T_ext = 250.0
     i_star = i_star_idx
 
     # Decide which callbacks to use:
@@ -97,113 +98,194 @@ begin
 
     total_biomass_df = vcat(total_biomass_df, DataFrame(herbivore_biomass = H_biomass, predator_biomass = P_biomass, total_biomass = H_biomass + P_biomass, i_star = i_star))
 end
+# end
 
-###############################################################################
-# 1) Build a DataFrame of final biomasses, and convert to abundances
-###############################################################################
-# We'll collect final data in a DataFrame with columns:
-#   :species, :type, :final_biomass, :bodyMass, :final_abundance
-
-df_final = DataFrame(
-    species         = String[],
-    type            = String[],   # "herb" or "pred"
-    final_biomass   = Float64[],
-    bodyMass        = Float64[],
-    final_abundance = Float64[]
-)
-
-# A) For herbivores, final biomass is H_data[i, end].
-for (idx, sp_name) in enumerate(herbivore_list)
-    final_bio = H_data[idx, end]
-    # If final_bio <= EXTINCTION_THRESHOLD, species effectively extinct => skip or keep as zero
-    if final_bio <= EXTINCTION_THRESHOLD
-        continue
-    end
-
-    # Find row in birmmals_biomass_fixed that matches species name
-    row_idx = findfirst(row -> row[:species] == sp_name, eachrow(birmmals_biomass_fixed))
-    if row_idx === nothing
-        @warn "Species $sp_name not found in birmmals_biomass_fixed; skipping."
-        continue
-    end
-
-    # Extract bodyMass
-    bm_val = birmmals_biomass_fixed[row_idx, :bodyMass]
-    # Convert biomass -> abundance
-    final_abund = final_bio / bm_val
-
-    push!(df_final, (
-        species         = sp_name,
-        type            = "herb",
-        final_biomass   = final_bio,
-        bodyMass        = bm_val,
-        final_abundance = final_abund
-    ))
-end
-
-# B) For predators, final biomass is P_data[α, end].
-for (pidx, sp_name) in enumerate(predator_list)
-    final_bio = P_data[pidx, end]
-    if final_bio <= EXTINCTION_THRESHOLD
-        continue
-    end
-
-    row_idx = findfirst(row -> row[:species] == sp_name, eachrow(birmmals_biomass_fixed))
-    if row_idx === nothing
-        @warn "Species $sp_name not found in birmmals_biomass_fixed; skipping."
-        continue
-    end
-
-    bm_val = birmmals_biomass_fixed[row_idx, :bodyMass]
-    final_abund = final_bio / bm_val
-
-    push!(df_final, (
-        species         = sp_name,
-        type            = "pred",
-        final_biomass   = final_bio,
-        bodyMass        = bm_val,
-        final_abundance = final_abund
-    ))
-end
-
-###############################################################################
-# 2) Sort in descending order of final_abundance
-###############################################################################
-df_final_sorted = sort(df_final, :final_abundance, rev=true)
-df_final_sorted_bio = sort(df_final, :final_biomass, rev=true)
-###############################################################################
-# 3) Plot a bar chart of final_abundance by species
-###############################################################################
 begin
-    log = false
+    # -------------------------------------------------------------------------
+    # 0) Set parameters
+    # -------------------------------------------------------------------------
+    include_predators   = false  
+    sort_by_abundance   = true  # Sort by abundance true or sort by biomass false
+    log                 = false  # Choose log scale
 
-    # Sort species by abundance in descending order
-    sorted_indices = sortperm(df_final_sorted.final_abundance, rev=true)
-    sorted_species = df_final_sorted.species[sorted_indices]
-    sorted_abundance = df_final_sorted.final_abundance[sorted_indices]
-    sorted_biomass = df_final_sorted.final_biomass[sorted_indices]
+    # -------------------------------------------------------------------------
+    # 1) Build a DataFrame of final biomasses, and convert to abundances
+    # -------------------------------------------------------------------------
+    df_final = DataFrame(
+        species         = String[],
+        type            = String[],   # "herb" or "pred"
+        final_biomass   = Float64[],
+        bodyMass        = Float64[],
+        final_abundance = Float64[]
+    )
 
-    fig_bar = Figure(resolution=(1500, 800))
+    # A) For herbivores, final biomass is H_data[i, end].
+    for (idx, sp_name) in enumerate(herbivore_list)
+        final_bio = H_data[idx, end]
+        if final_bio <= EXTINCTION_THRESHOLD
+            continue
+        end
 
-    # Plot abundance
-    ax_abundance = Axis(fig_bar[1, 1], xlabel="Species", ylabel="Abundance",
-                        title="Species Abundance (last time step)",
-                        yscale=log ? log10 : identity)
-    species_indices = 1:length(sorted_species)  # Numeric indices for sorted species
-    barplot!(ax_abundance, species_indices, sorted_abundance)
-    ax_abundance.xticks = (species_indices, sorted_species)
-    ax_abundance.xticklabelrotation = π / 2  # Rotate x-tick labels 90 degrees
+        row_idx = findfirst(row -> row[:species] == sp_name, eachrow(birmmals_biomass_fixed))
+        if row_idx === nothing
+            @warn "Species $sp_name not found in birmmals_biomass_fixed; skipping."
+            continue
+        end
 
-    # Plot biomass
-    ax_biomass = Axis(fig_bar[1, 2], xlabel="Species", ylabel="Biomass",
-                      title="Species Biomass (last time step)",
-                      yscale=log ? log10 : identity)
-    barplot!(ax_biomass, species_indices, sorted_biomass)
-    ax_biomass.xticks = (species_indices, sorted_species)
-    ax_biomass.xticklabelrotation = π / 2  # Rotate x-tick labels 90 degrees
+        bm_val = birmmals_biomass_fixed[row_idx, :bodyMass]
+        final_abund = final_bio / bm_val
+
+        push!(df_final, (
+            species         = sp_name,
+            type            = "herb",
+            final_biomass   = final_bio,
+            bodyMass        = bm_val,
+            final_abundance = final_abund
+        ))
+    end
+
+    # B) For predators (only if include_predators is true)
+    if include_predators
+        for (pidx, sp_name) in enumerate(predator_list)
+            final_bio = P_data[pidx, end]
+            if final_bio <= EXTINCTION_THRESHOLD
+                continue
+            end
+
+            row_idx = findfirst(row -> row[:species] == sp_name, eachrow(birmmals_biomass_fixed))
+            if row_idx === nothing
+                @warn "Species $sp_name not found in birmmals_biomass_fixed; skipping."
+                continue
+            end
+
+            bm_val = birmmals_biomass_fixed[row_idx, :bodyMass]
+            final_abund = final_bio / bm_val
+
+            push!(df_final, (
+                species         = sp_name,
+                type            = "pred",
+                final_biomass   = final_bio,
+                bodyMass        = bm_val,
+                final_abundance = final_abund
+            ))
+        end
+    end
+
+    # -------------------------------------------------------------------------
+    # 2) Split df_final into herbivores and predators
+    # -------------------------------------------------------------------------
+    herb_mask = df_final.type .== "herb"
+    pred_mask = df_final.type .== "pred"  # will be empty if include_predators=false
+
+    # -- Sort herbivores
+    if sort_by_abundance
+        sorted_herb_inds = sortperm(df_final[herb_mask, :final_abundance], rev=true)
+    else
+        sorted_herb_inds = sortperm(df_final[herb_mask, :final_biomass], rev=true)
+    end
+
+    herb_species_sorted    = df_final[herb_mask, :species][sorted_herb_inds]
+    herb_abundance_sorted  = df_final[herb_mask, :final_abundance][sorted_herb_inds]
+    herb_biomass_sorted    = df_final[herb_mask, :final_biomass][sorted_herb_inds]
+
+    # -- Sort predators (only if include_predators)
+    pred_species_sorted    = String[]
+    pred_abundance_sorted  = Float64[]
+    pred_biomass_sorted    = Float64[]
+    if include_predators && !isempty(df_final[pred_mask, :species])
+        if sort_by_abundance
+            sorted_pred_inds = sortperm(df_final[pred_mask, :final_abundance], rev=true)
+        else
+            sorted_pred_inds = sortperm(df_final[pred_mask, :final_biomass], rev=true)
+        end
+
+        pred_species_sorted    = df_final[pred_mask, :species][sorted_pred_inds]
+        pred_abundance_sorted  = df_final[pred_mask, :final_abundance][sorted_pred_inds]
+        pred_biomass_sorted    = df_final[pred_mask, :final_biomass][sorted_pred_inds]
+    end
+
+    # -------------------------------------------------------------------------
+    # 3) Create figure with either a 1×2 or 2×2 layout
+    # -------------------------------------------------------------------------
+    # If you prefer always a 2×2 figure (even if predators are not included),
+    # you can omit this if-else and just do a 2×2. For demonstration, we make
+    # the figure "shorter" when not including predators.
+    # -------------------------------------------------------------------------
+    if include_predators
+        fig_bar = Figure(resolution=(1500, 1200))
+    else
+        fig_bar = Figure(resolution=(1500, 900))
+    end
+
+    # -------------------------------------------------------------------------
+    # 3A) Top row: Herbivore Abundance and Biomass
+    # -------------------------------------------------------------------------
+    # Herbivore biomass
+    ax_biomass_herb = Axis(
+        fig_bar[1, 1],
+        # xlabel="Herbivore Species",
+        ylabel="Biomass",
+        title="Herbivores: Biomass",
+        yscale=log ? log10 : identity
+    )
+    herb_indices = 1:length(herb_species_sorted)
+    barplot!(ax_biomass_herb, herb_indices, herb_biomass_sorted)
+    ax_biomass_herb.xticks = (herb_indices, herb_species_sorted)
+    ax_biomass_herb.xticklabelrotation = π/2.5
+    
+    # Herbivore abundance
+    ax_abundance_herb = Axis(
+        fig_bar[1, 2],
+        # xlabel="Herbivore Species",
+        ylabel="Abundance",
+        title="Herbivores: Abundance",
+        yscale=log ? log10 : identity
+    )
+    herb_indices = 1:length(herb_species_sorted)
+    barplot!(ax_abundance_herb, herb_indices, herb_abundance_sorted)
+    ax_abundance_herb.xticks = (herb_indices, herb_species_sorted)
+    ax_abundance_herb.xticklabelrotation = π/2.5
+
+    # -------------------------------------------------------------------------
+    # 3B) Bottom row: Predator Abundance and Biomass (only if include_predators)
+    # -------------------------------------------------------------------------
+    if include_predators && !isempty(pred_species_sorted)
+        ax_biomass_pred = Axis(
+            fig_bar[2, 1],
+            # xlabel="Predator Species",
+            ylabel="Biomass",
+            title="Predators: Biomass",
+            yscale=log ? log10 : identity
+        )
+        barplot!(ax_biomass_pred, pred_indices, pred_biomass_sorted)
+        ax_biomass_pred.xticks = (pred_indices, pred_species_sorted)
+        ax_biomass_pred.xticklabelrotation = π/2.5
+
+        ax_abundance_pred = Axis(
+            fig_bar[2, 2],
+            # xlabel="Predator Species",
+            ylabel="Abundance",
+            title="Predators: Abundance",
+            yscale=log ? log10 : identity
+        )
+        pred_indices = 1:length(pred_species_sorted)
+        barplot!(ax_abundance_pred, pred_indices, pred_abundance_sorted)
+        ax_abundance_pred.xticks = (pred_indices, pred_species_sorted)
+        ax_abundance_pred.xticklabelrotation = π/2.5
+    end
 
     display(fig_bar)
 end
 
+H_i0
+# Find the cell with highest richness excluding NaNs
+filtered_richness = filter(!isnan, DA_richness[:])
+max_richness = maximum(filtered_richness)
+max_richness_idx = findfirst(isequal(max_richness), DA_richness)
+max_richness_cell = CartesianIndex(max_richness_idx.I...)
+max_richness_idx_in_idx = findfirst(isequal(max_richness_idx), idx)
+println("Cell with highest richness excluding NaNs: ", max_richness_cell)
 
-
+perro = localHatH
+perro = sort(perro, rev = true)
+barplot(perro)
