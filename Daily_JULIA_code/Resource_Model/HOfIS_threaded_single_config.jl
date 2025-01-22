7d09f6e23d0f6f3da266ad02e3113f824b76b833
Nico Merino <nicolasmerino41@gmail.com>
2025-01-22 13:33:27 +0100

    Update HOfIS_threaded_single_config.jl

--- Daily_JULIA_code/Resource_Model/HOfIS_threaded_single_config.jl
+++ Daily_JULIA_code/Resource_Model/HOfIS_threaded_single_config.jl
@@ -14,7 +14,6 @@ include("Functions/Callbacks_function.jl")
 include("npp_DA_relative_to_1000.jl")
 include("Functions/Computing_metrics.jl")
 include("../HerbivoresVsPredators/Exploring HerbPred metaweb composition.jl")
-include("Remaking_iberian_interact_NA.jl")
 
 global EXTINCTION_THRESHOLD = 1e-6
 global T_ext               = 250.0
@@ -50,7 +49,7 @@ param_combinations = Random.shuffle!(param_combinations)
 param_combinations = param_combinations[1:2000]
 
 # 3) Prepare DataFrame for storing best results
-best_params_24_cells_fixed_mu05_nppDA1000 = DataFrame(
+best_params_24_cells_fixed_mu05_2000iter = DataFrame(
     cell_id                 = Int[],
     i                       = Int[],
     j                       = Int[],
@@ -60,6 +59,7 @@ best_params_24_cells_fixed_mu05_nppDA1000 = DataFrame(
     symmetrical_competition = Bool[],
     NPP                     = Float64[],
     g_iH_i                  = Float64[],
+    g_iH_i_over_NPP         = Float64[],
     survived_herbivores     = Int[],
     survived_predators      = Int[],
     total_survivors         = Int[],
@@ -76,26 +76,34 @@ best_params_24_cells_fixed_mu05_nppDA1000 = DataFrame(
 global_lock = ReentrantLock()
 
 # Constants
-MAX_ITERS                  = 2000      # Up to 2000 combos
+MAX_ITERS                  = 100      # Up to 2000 combos
 SURVIVAL_THRESHOLD         = 0.0      # We'll store the best if it reaches at least 0.75
 
-@time Threads.@threads for cell in 1  # or whichever cells you want
+@time Threads.@threads for cell in 1:24  # or whichever cells you want
     local_i, local_j = idx[cell][1], idx[cell][2]
     @info "Processing cell $cell (i=$local_i, j=$local_j)..."
+
+    println("Thread $(Threads.threadid()) is processing cell $cell")
 
     # Gather cell data
     sp_nm                 = extract_species_names_from_a_cell(DA_birmmals_with_pi[local_i, local_j])
     local_S, local_R      = identify_n_of_herbs_and_preds(sp_nm)
     predator_has_prey     = check_predator_has_prey(sp_nm)
+
     if !predator_has_prey[1]
        local_R -= predator_has_prey[2]
        filter!(name -> !(name in predator_has_prey[3]), sp_nm)
-       @info("In cell $cell, we removed $(predator_has_prey[2]) predators without prey.")
+       @info("In cell $cell, we removed $(predator_has_prey[2]) predators: $(predator_has_prey[3]).")
+       if predator_has_prey[2] > 0
+        species_names = sp_nm
+       else
+        species_names = nothing # If we can find all predators, we pass species_names = nothing cause setup_community_from_cell will generate it
+       end
     end
     
     localNPP              = Float64(npp_DA_relative_to_1000[local_i, local_j]) 
     localH0_vector        = Vector{Float64}(H0_DA[local_i, local_j].a)
-    cb_no_trigger, cb_trg = build_callbacks(local_S, local_R, EXTINCTION_THRESHOLD, T_ext, 1)
+    # cb_no_trigger, cb_trg = build_callbacks(local_S, local_R, EXTINCTION_THRESHOLD, T_ext, 1)
 
     # We'll track the best result so far in local variables
     best_survival_rate  = 0.0
@@ -105,13 +113,17 @@ SURVIVAL_THRESHOLD         = 0.0      # We'll store the best if it reaches at le
     # 4) Up to 2000 combos (or fewer, if param_combinations < 2000)
     for (p_idx, combo) in enumerate(param_combinations[1:min(end,MAX_ITERS)])
         mu_val, mu_pred_val, eps_val, sym_competition = combo
-
+        
+        if p_idx % 10 == 0
+            println("Thread $(Threads.threadid()) is processing cell $cell, combo $p_idx")
+        end  
         # Attempt setup
         results = attempt_setup_community(
             local_i, local_j,
             mu_val, mu_pred_val, eps_val, sym_competition;
             localNPP      = localNPP,
-            localH0_vector= localH0_vector
+            localH0_vector= localH0_vector,
+            species_names = species_names
         )
         if results === nothing
             continue
@@ -144,7 +156,7 @@ SURVIVAL_THRESHOLD         = 0.0      # We'll store the best if it reaches at le
 
         # Wrap the solver call in the logging context:
         sol = with_logger(logger) do
-            solve(prob, Tsit5(); callback=cb_no_trigger, abstol=1e-8, reltol=1e-6)
+            solve(prob, Tsit5(); abstol=1e-8, reltol=1e-6)
         end
         ###############################################
 
@@ -161,9 +173,12 @@ SURVIVAL_THRESHOLD         = 0.0      # We'll store the best if it reaches at le
         total_surv     = survived_herb + survived_pred
         total_species  = S2 + R2
         survival_rate  = total_surv / total_species
+        giHi           = sum(g_i .* H_end)
+        ratio_ok       = (giHi / localNPP > 0.5) && (giHi / localNPP < 10.0)
+        
 
         # If better than what's stored, update best result
-        if survival_rate > best_survival_rate
+        if survival_rate > best_survival_rate && ratio_ok
             # compute the rest:
             herbivore_survival_rate = survived_herb / S2
             predator_survival_rate  = survived_pred / R2
@@ -171,8 +186,7 @@ SURVIVAL_THRESHOLD         = 0.0      # We'll store the best if it reaches at le
             P_biomass               = sum(P_end)
             biomass_at_the_end      = H_biomass + P_biomass
             ratio                   = (H_biomass == 0.0) ? NaN : (P_biomass / H_biomass)
-            giHi                    = sum(g_i .* H_end)
-
+            
             best_survival_rate = survival_rate
             best_result = (
                 cell_id                 = cell,
@@ -184,6 +198,7 @@ SURVIVAL_THRESHOLD         = 0.0      # We'll store the best if it reaches at le
                 symmetrical_competition = sym_competition,
                 NPP                     = localNPP,
                 g_iH_i                  = giHi,
+                g_iH_i_over_NPP         = round(giHi / localNPP, digits=4), 
                 survived_herbivores     = survived_herb,
                 survived_predators      = survived_pred,
                 total_survivors         = total_surv,
@@ -198,11 +213,14 @@ SURVIVAL_THRESHOLD         = 0.0      # We'll store the best if it reaches at le
             )
         end
 
-        # If found full survival => record immediately, break
+        # Early stop if full survival + the ratio check
         if isapprox(survival_rate, 1.0; atol=1e-10)
-            @info "Cell $cell => full survival with (mu=$mu_val, mu_pred=$mu_pred_val, eps=$eps_val). Stopping early."
-            found_full_survival = true
-            break
+            ratio_ok = (giHi / localNPP > 0.5) && (giHi / localNPP < 10.0)
+            if ratio_ok
+                @info "Cell $cell => full survival with (mu=$mu_val, mu_pred=$mu_pred_val, eps=$eps_val). Stopping early."
+                found_full_survival = true
+                break
+            end
         end
     end
 
@@ -210,22 +228,22 @@ SURVIVAL_THRESHOLD         = 0.0      # We'll store the best if it reaches at le
     if found_full_survival
         # best_result *should* be 1.0
         @lock global_lock begin
-            push!(best_params_24_cells_fixed_mu05_nppDA1000, best_result)
+            push!(best_params_24_cells_fixed_mu05_2000iter, best_result)
         end
     else
         # If not found full survival, check if best >= 0.75
         if best_survival_rate >= SURVIVAL_THRESHOLD  # e.g. 0.75
             @lock global_lock begin
-                push!(best_params_24_cells_fixed_mu05_nppDA1000, best_result)
+                push!(best_params_24_cells_fixed_mu05_2000iter, best_result)
             end
         else
-            @info "Cell $cell: best survival was $(round(best_survival_rate, digits=2)) < 0.75 => Not storing."
+            @info "Cell $cell: best survival was $(round(best_survival_rate, digits=2)) < $SURVIVAL_THRESHOLD => Not storing."
         end
     end
 end  # end Threads.@threads
 
 # done
-CSV.write("Daily_JULIA_code/Resource_Model/Best_params_&_other_outputs/best_params_24_cells_fixed_mu05_nppDA1000.csv", best_params_24_cells_fixed_mu05_nppDA1000)
+CSV.write("Daily_JULIA_code/Resource_Model/Best_params_&_other_outputs/best_params_24_cells_fixed_mu05_2000iter.csv", best_params_24_cells_fixed_mu05_2000iter)
 @info "Finished. We stored combos with survival >= 0.75, or full survival."
 best_params_24_cells_fixed_mu05 = CSV.read("best_results_per_cell_limited_to_2000.csv", DataFrame)
 
