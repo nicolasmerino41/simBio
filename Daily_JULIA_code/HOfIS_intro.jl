cb9820bdacf29302ad29db7c9d9fd046ab49a026
nicomerino <nicolasmerino41@gmail.com>
2025-02-20 17:30:57 +0100

    Added NewFramework logic

--- Daily_JULIA_code/HOfIS_intro.jl
+++ Daily_JULIA_code/HOfIS_intro.jl
@@ -12,6 +12,9 @@ include("Resource_Model/npp_DA_relative_to_1000.jl")
 include("Resource_Model/Functions/Computing_metrics.jl")
 include("HerbivoresVsPredators/Exploring HerbPred metaweb composition.jl")
 include("Resource_Model/Functions/attempt_feasibility.jl")
+include("Resource_Model/NewFramework/New_dynamics!.jl")
+include("Resource_Model/NewFramework/New_FI_functions.jl")
+include("Resource_Model/NewFramework/New_attempt_setup_community.jl")
 global EXTINCTION_THRESHOLD = 1e-6
 global T_ext               = 250.0
 include("Resource_Model/SingleModelRun.jl")
