c8a5a2193519043e0a389125deffb26846006e09
Nico Merino <nicolasmerino41@gmail.com>
2025-03-31 18:54:35 +0200

    Update PlottingEigenvalues.jl

--- Daily_JULIA_code/Resource_Model/NewFramework/Omnivory/Anal_pipeline/PlottingEigenvalues.jl
+++ Daily_JULIA_code/Resource_Model/NewFramework/Omnivory/Anal_pipeline/PlottingEigenvalues.jl
@@ -1,7 +1,6 @@
 # Assume A_j is the Jacobian and A_p is the parameter tuple containing S and R,
 # and that sp_nm is the ordered species list: first S entries for herbivores (and omnivores), then predators.
 # Also assume that omnivore_names and predator_names are defined.
-
 S = A_p.S
 R = A_p.R
 n = S + R
@@ -13,7 +12,8 @@ vecs = ev.vectors
 
 # Create a vector to store guild labels for each eigenvalue
 guild_labels = Vector{String}(undef, n)
-sp_nm = extract_species_names_from_a_cell(DA_birmmals_with_pi_corrected[idx[2][1], idx[2][2]])
+sp_nm = extract_species_names_from_a_cell(DA_birmmals_with_pi_corrected[idx[cell][1], idx[cell][2]])
+# sp_nm = vcat(A_eq.herbivore_list, A_eq.predator_list)
 # For each eigenvector, find the index of the largest magnitude component.
 for i in 1:n
     v = vecs[:, i]
@@ -39,10 +39,17 @@ println(sort(re_vals))
 im_vals = imag.(vals)
 
 # Plot eigenvalues in the complex plane.
-Plots.scatter(re_vals, im_vals, xlabel="Real Part", ylabel="Imaginary Part",
-        title="Eigenvalue Plot by Guild", color=colors, marker=:circle)
-hline!([0], linestyle=:dash, color=:black)
-vline!([0], linestyle=:dash, color=:black)
+begin
+    fig = Figure()
+    ax = Axis(
+        fig[1, 1], xlabel = "Real Part", ylabel = "Imaginary Part",
+        title = "Eigenvalue Plot by Guild"
+    )
+    MK.scatter!(ax, re_vals, im_vals, color = colors, marker = :circle)
+    MK.hlines!(ax, [0], linestyle = :dash, color = :black)
+    MK.vlines!(ax, [0], linestyle = :dash, color = :black)
+    display(fig)
+end
 
 J = A_j
 # Suppose J is the Jacobian matrix computed at equilibrium (for example, from ForwardDiff.jacobian)
@@ -56,5 +63,13 @@ println("Sensitivity matrix S(t) = exp(J*t):")
 println(S_t)
 
 # Optionally, if you want to visualize the sensitivity matrix as a heatmap:
-Plots.heatmap(S_t, title = "Sensitivity Matrix at t = $t_val", xlabel = "Initial State Index", ylabel = "State Index at t")
-
+begin
+    fig = Figure()
+    ax = Axis(fig[1, 1],
+        xlabel = "Initial State Index",
+        ylabel = "State Index at t",
+        title = "Sensitivity Matrix at t = $t_val")
+    heatmap!(ax, S_t)
+    Colorbar(fig[1, 2], limits = (minimum(S_t), maximum(S_t)), colormap = cgrad(:viridis))
+    display(fig)
+end
