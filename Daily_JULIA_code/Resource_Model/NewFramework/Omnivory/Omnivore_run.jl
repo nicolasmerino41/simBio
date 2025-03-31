7df1084fd76a73a90955d4d07974c7ac10797d13
Nico Merino <nicolasmerino41@gmail.com>
2025-03-31 18:55:37 +0200

    Update Omnivore_run.jl

--- Daily_JULIA_code/Resource_Model/NewFramework/Omnivory/Omnivore_run.jl
+++ Daily_JULIA_code/Resource_Model/NewFramework/Omnivory/Omnivore_run.jl
@@ -25,7 +25,7 @@ function omnivore_run(
     use_cb = false,
     perturbation_halfway = false,
     species_to_perturb = 0,
-    removal_fraction = 0.0
+    removal_fraction = 0.0,
 
 )
     AAAA = DataFrame()
@@ -123,14 +123,7 @@ function omnivore_run(
     else
         u0 = H_init
     end
-    if perturbation_halfway && species_to_perturb > 0 && species_to_perturb <= S+R
-        u0[species_to_perturb] -= u0[species_to_perturb] * removal_fraction
-    elseif perturbation_halfway && species_to_perturb > S+R
-        error("Error: species_to_perturb > S+R")
-    elseif perturbation_halfway && iszero(species_to_perturb)
-        error("Error: You activated perturbation_halfway but didn't specify species_to_perturb")
-    end
-
+    
     # Build the parameter tuple for the ODE.
     # Order: (S, R, K_i, r_i, mu, nu, P_matrix, O_matrix, T_matrix, epsilon, m_alpha, K_alpha, B_matrix, D_matrix)
     if !isnothing(force_nu_to)
@@ -156,9 +149,26 @@ function omnivore_run(
         end
     end
 
+    # --- PERTURBATION HALFWAY ---
+    if perturbation_halfway && species_to_perturb > 0 && species_to_perturb <= S+R
+        u0[species_to_perturb] -= u0[species_to_perturb] * removal_fraction
+        new_u0 = deepcopy(sol[:, end])
+        new_u0[species_to_perturb] -= new_u0[species_to_perturb] * removal_fraction
+        prob = ODEProblem(omnivore_dynamics!, new_u0, (time_end, time_end+time_end), params)
+        if use_cb
+            new_sol = solve(prob, Tsit5(); callback = cb_no_trigger, abstol = 1e-8, reltol = 1e-6)
+            else
+            new_sol = solve(prob, Tsit5(); abstol = 1e-8, reltol = 1e-6)
+        end
+    elseif perturbation_halfway && species_to_perturb > S+R
+        error("Error: species_to_perturb > S+R")
+    elseif perturbation_halfway && iszero(species_to_perturb)
+        error("Error: You activated perturbation_halfway but didn't specify species_to_perturb")
+    end
+
     # --- Plotting the Dynamics using Makie ---
     if plot    
-        fig = Figure(; size = (600, 500))
+        fig = Figure(; size = (900, 700))
         ax = Axis(
             fig[1, 1],
             xlabel = "Time",
@@ -180,6 +190,44 @@ function omnivore_run(
         # Plot predator dynamics (indices S+1:S+R) in red dashed lines.
         for alpha in 1:R
             lines!(ax, times_combined, sol[S+alpha, :], label = "P$(alpha)", linestyle = :dash, color = :red)
+        end
+
+        if perturbation_halfway
+            ax2 = Axis(
+                fig[1, 2],
+                xlabel = "Time",
+                ylabel = "Biomass",
+                title = "Dynamics for cell after perturbation",
+            )
+            times_combined = new_sol.t
+            # Plot herbivore/omnivore dynamics: species that are in herbivore_names are blue; those in omnivore_names are green.
+            for i in 1:S
+                if herbivore_list[i] in omnivore_names
+                    lines!(ax2, times_combined, new_sol[i, :], label = "O$(i)", color = :green)
+                    # println("O$(i)")
+                else
+                    lines!(ax2, times_combined, new_sol[i, :], label = "H$(i)", color = :blue)
+                    # println("H$(i)")
+                end
+            end
+            
+            # Plot predator dynamics (indices S+1:S+R) in red dashed lines.
+            for alpha in 1:R
+                lines!(ax2, times_combined, new_sol[S+alpha, :], label = "P$(alpha)", linestyle = :dash, color = :red)
+            end
+            
+            ax3 = Axis(
+                fig[2, 1],
+                xlabel = "Time",
+                ylabel = "Total Biomass",
+                title = "Dynamics for cell before and after perturbation",
+            )
+            times_combined = vcat(sol.t, new_sol.t)
+            # println("times combined: ", length(times_combined))
+            # println("combined sol: ", typeof(hcat(sol[:, :], new_sol[:, :])))
+            # println("combined sol: ", size(hcat(sol[:, :], new_sol[:, :])))
+            # println("sum is ", sum(eachrow(hcat(sol[:, :], new_sol[:, :]))))
+            lines!(ax3, times_combined, sum(eachrow(hcat(sol[:, :], new_sol[:, :]))))
         end
         display(fig)
     end
@@ -209,17 +257,15 @@ function omnivore_run(
         survival_rate           = survival_rate,
         h_survival              = "$(survived_herb)/$S",
         p_survival              = "$(survived_pred)/$R",
-        H_eq                    = [H_eq],
-        P_eq                    = [P_eq],
-        H_star                  = [H_star],
-        P_star                  = [P_star],
+        biomass_at_the_end      = biomass_at_the_end,
         H_vector                = [H_end],
         P_vector                = [P_end],
+        H_eq                    = [H_eq],
+        P_eq                    = [P_eq],
         survived_herbivores     = survived_herb,
         survived_predators      = survived_pred,
         H_biomass               = H_biomass,
         P_biomass               = P_biomass,
-        biomass_at_the_end      = biomass_at_the_end,
         herb_pred_ratio         = ratio,
         total_survivors         = total_surv,
         total_species           = total_species,
@@ -232,11 +278,79 @@ function omnivore_run(
     )
     
     append!(AAAA, single_run_results)
+
+    if perturbation_halfway
+    
+        new_H_end = new_sol[1:S, end]
+        new_P_end = new_sol[S+1:S+R, end]
+        new_H_end = map(x -> x < EXTINCTION_THRESHOLD ? 0.0 : x, new_H_end)
+        new_P_end = map(x -> x < EXTINCTION_THRESHOLD ? 0.0 : x, new_P_end)
+        new_survived_herb = count(x -> x > EXTINCTION_THRESHOLD, new_H_end)
+        new_survived_pred = count(x -> x > EXTINCTION_THRESHOLD, new_P_end)
+        new_total_surv = new_survived_herb + new_survived_pred
+        new_total_species = S + R
+        new_survival_rate = new_total_surv / new_total_species
+        new_herbivore_survival_rate = new_survived_herb / S 
+        new_predator_survival_rate = (R > 0 ? new_survived_pred / R : 0.0)
+        new_H_biomass = sum(new_H_end)
+        new_P_biomass = sum(new_P_end)
+        new_biomass_at_the_end = new_H_biomass + new_P_biomass
+        new_ratio = (new_H_biomass == 0.0 ? NaN : (new_P_biomass / new_H_biomass))
+
+        new_single_run_results = DataFrame(
+            cell_id                 = cell,
+            i                       = local_i,
+            j                       = local_j,
+            survival_rate           = new_survival_rate,
+            h_survival              = "$(new_survived_herb)/$S",
+            p_survival              = "$(new_survived_pred)/$R",
+            biomass_at_the_end      = new_biomass_at_the_end,
+            H_vector                = [new_H_end],
+            P_vector                = [new_P_end],
+            H_eq                    = [H_eq],
+            P_eq                    = [P_eq],
+            survived_herbivores     = new_survived_herb,    
+            survived_predators      = new_survived_pred,
+            H_biomass               = new_H_biomass,
+            P_biomass               = new_P_biomass,
+            herb_pred_ratio         = new_ratio,
+            total_survivors         = new_total_surv,
+            total_species           = new_total_species,
+            herbivore_survival_rate = new_herbivore_survival_rate,
+            predator_survival_rate  = new_predator_survival_rate,
+            mu                      = mu_val,
+            nu                      = nu_val,
+            epsilon_val             = eps_val,
+            mean_m_alpha            = mean_m_alpha
+        )
+        
+        append!(AAAA, new_single_run_results)
+    end
     
     initial_biomass = sum(u0)
     final_biomass = sum(sol[:, end])
     println("Initial biomass was $initial_biomass and final biomass is $final_biomass")
 
+    if perturbation_halfway
+        if round(new_biomass_at_the_end, digits=0) == round(biomass_at_the_end, digits=0)
+            println("The total biomass after the perturbation did not change.")
+            if all(round.(vcat(new_H_end, new_P_end), digits=0) .== round.(vcat(H_end, P_end), digits=0))
+                println("The equilibrium biomass after the perturbation did not change.")
+            end
+        else
+            println("The total biomass after the perturbation changed and obviously so did the equilibrium biomasses.")
+            println("Original biomass: $biomass_at_the_end, new biomass: $new_biomass_at_the_end")
+        end
+        if new_herbivore_survival_rate != herbivore_survival_rate || new_predator_survival_rate != predator_survival_rate
+            println("The survival rates after the perturbation changed.")
+        end        
+    end
+    diff = []
+    for i in 2:size(new_sol, 2)
+        # println("The biomass respect to equilibrium is ", abs(sum(new_sol[:, i]) - biomass_at_the_end))
+        push!(diff, abs(sum(new_sol[:, i]) - biomass_at_the_end))
+    end
+    println("The maximum absolute difference is ", maximum(diff), " and it was found at time ", new_sol.t[argmax(diff)])
     if do_you_want_params && do_you_want_sol
         return AAAA, params, sol
     elseif do_you_want_params || do_you_want_sol
@@ -247,20 +361,21 @@ function omnivore_run(
 
 end
 
-cb_no_trigger, cb_trigger = build_callbacks(37, 8, EXTINCTION_THRESHOLD, T_ext, 1)
+# cb_no_trigger, cb_trigger = build_callbacks(37, 8, EXTINCTION_THRESHOLD, T_ext, 1)
+for i in 1:10
 @time A_run = omnivore_run(
     1, # cell
-    1.0, 0.31, 0.1; # mu, epsilon, m_alpha
+    1.0, 1.0, 0.1; # mu, epsilon, m_alpha
     delta_nu = 0.05,
     d_alpha = 1.0, d_i = 1.0,
-    time_end = 10000.0,
+    time_end = 1000.0,
     do_you_want_params = false,
     do_you_want_sol = false,
     include_predators = true,
     include_omnivores = true,
     plot = true,
     sp_removed_name = nothing,
-    artificial_pi = false, pi_size = 10.0,
+    artificial_pi = true, pi_size = 10.0,
     H_init = nothing,
     P_init = nothing,
     ignore_inf_error = true,
@@ -270,15 +385,11 @@ cb_no_trigger, cb_trigger = build_callbacks(37, 8, EXTINCTION_THRESHOLD, T_ext, 
     include_n_omnivores = 0, # leave at 0 for no effect
     nu_omni_proportion = 1.0, # leave at 1.0 for no effect
     nu_b_proportion = 1.0, # leave at 1.0 for no effect
-    r_omni_proportion = 1.0,
+    r_omni_proportion = 1.0, # leave at 1.0 for no effect
     force_nu_to = nothing, # leave at nothing for no effect
     use_cb = false,
     perturbation_halfway = true,
-    species_to_perturb = 3,
+    species_to_perturb = i,
     removal_fraction = 1.0
 )
-
-println("Survival rate: ", A_run.survival_rate[1])
-
-println(sol[:, end])
-println(sol.u[:, end])
+end
