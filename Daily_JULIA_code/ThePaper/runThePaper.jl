41307fb3033c2782acc5956424afb109dd5bdd8e
nicomerino <nicolasmerino41@gmail.com>
2025-04-09 12:07:36 +0200

    Create runThePaper.jl
    This is a startup script to avoid having to use the ResourceModel run.jl

---
+++ Daily_JULIA_code/ThePaper/runThePaper.jl
@@ -0,0 +1,23 @@
+begin
+    using Pkg
+    Pkg.activate(joinpath("C:\\Users", PC, "OneDrive\\PhD\\GitHub\\simBio"))
+    cd(joinpath("C:\\Users", PC, "OneDrive\\PhD\\GitHub\\simBio"))
+    meta_path = joinpath("C:\\Users", PC, "OneDrive\\PhD\\Metaweb Modelling")
+    using ArchGDAL #, Shapefile, NCDatasets
+    using CSV, DataFrames, NaNMath
+    using NamedArrays, StaticArrays, OrderedCollections
+    using Rasters, RasterDataSources #, DimensionalData
+    using DynamicGrids, Dispersal
+    using Dates, Distributions, Serialization, StatsBase, JLD2, Random
+    using ColorSchemes, Colors #Crayons, 
+    using Makie, CairoMakie # ImageMagick
+    if ode_true
+        using DifferentialEquations, DiffEqCallbacks, LinearAlgebra, Logging, ForwardDiff
+    end
+    # if end_true
+    #     using DifferentialEquations # EcologicalNetworksDynamics, 
+    # end
+    # using Plots
+    const DG, MK, AG, RS, Disp, DF = DynamicGrids, Makie, ArchGDAL, Rasters, Dispersal, DataFrames
+    const COLORMAPS = [:magma, :viridis, :cividis, :inferno, :delta, :seaborn_icefire_gradient, :seaborn_rocket_gradient, :hot]
+end
