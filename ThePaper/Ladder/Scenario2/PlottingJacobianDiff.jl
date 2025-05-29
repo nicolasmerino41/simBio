ba5adc70fd48b93d0202bdc6389a32f90fd45f5a
Nico Merino <nicolasmerino41@gmail.com>
2025-05-29 18:37:06 +0200

    Update PlottingJacobianDiff.jl

--- ThePaper/Ladder/Scenario2/PlottingJacobianDiff.jl
+++ ThePaper/Ladder/Scenario2/PlottingJacobianDiff.jl
@@ -1,18 +1,38 @@
-
-using DataFrames
+step_keys = [
+        "S1","S2","S3","S4","S5","S6", "S7", "S8"]
+        # "S7"
+    # ]
+step_names = [
+    "Full Model", "Global A (Global ϵ)", " Global AE",
+    "Randomize m_cons ↻", "Randomize ξ̂ ↻", "Randomize K_res ↻",
+    "Global A (Global ϵ) Mean B", "Global AE Mean B"
+]
 
 res_cols = Symbol.("resilience_" .* step_keys)
-dff = filter(row -> all(row[c] < 0 for c in res_cols), dff)
-println("subset size: ", nrow(dff))
-step_numbers = [1,3,4,5,6,7,8] # Numeric steps
-n = nrow(dff)
+df = filter(row -> all(row[c] < 0 for c in res_cols), df)
+println("subset size: ", nrow(df))
+step_numbers = [1,2,3,4,5,6,7,8] # Numeric steps
+n = nrow(df)
 
 long_jdiff = DataFrame(
     Step = repeat(step_numbers, inner=n),
-    J_diff = vcat([dff[!, Symbol("J_diff_S$(s)")] for s in step_numbers]...)
+    J_diff = vcat([df[!, Symbol("J_diff_S$(s)")] for s in step_numbers]...)
 )
+begin
+    fig = Figure(size=(800, 650))
+    ax = Axis(fig[1,1], ylabel="Jacobian diff (Frobenius norm) logScale", xlabel="Step", title="Jacobian diff per step", yscale=log10,
+        titlealign = :center, ylabelsize = 18, xlabelsize = 18, xticklabelrotation = π/4,
+        xticks = (step_numbers, step_names), )
+    boxplot!(ax, long_jdiff.Step, long_jdiff.J_diff)
+    display(fig)    
+end
 
-fig = Figure(size=(800, 400))
-ax = Axis(fig[1,1], ylabel="Jacobian diff (Frobenius norm)", xlabel="Step", title="Jacobian diff per step")
-boxplot!(ax, long_jdiff.Step, long_jdiff.J_diff)
-display(fig)
+begin
+    fig = Figure(size=(800, 650))
+    ax = Axis(fig[1,1], ylabel="Jacobian diff (Frobenius norm)", xlabel="Step", title="Jacobian diff per step",
+        titlealign = :center, ylabelsize = 18, xlabelsize = 18, xticklabelrotation = π/4,
+        xticks = (step_numbers, step_names), )
+    boxplot!(ax, long_jdiff.Step, long_jdiff.J_diff)
+    display(fig)    
+end
+
