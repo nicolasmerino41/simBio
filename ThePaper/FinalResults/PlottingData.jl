49a1105bbd3b4da56cf1fbafeec22e0735461e57
nicomerino <nicolasmerino41@gmail.com>
2025-07-10 19:14:48 +0200

    Create PlottingData.jl

---
+++ ThePaper/FinalResults/PlottingData.jl
@@ -0,0 +1,44 @@
+G = K
+
+step_keys = ["_full","_S1","_S2","_S3","_S4"]
+res_cols = Symbol.("resilience" .* step_keys)
+G = filter(row -> all(row[c] < 0 for c in res_cols), G)
+println("subset size: ", nrow(G))
+min_d_cols = Symbol.("sigma_over_min_d" .* step_keys)
+G = filter(row -> all(row[c] < 10.0 for c in min_d_cols), G)
+ 
+G = filter(row -> all(x -> !(x isa AbstractFloat) || (!isnan(x) && !isinf(x)), row), G)
+sl_cols = Symbol.("SL" .* step_keys)
+G = filter(row -> all(all(x -> x < 1000, row[c]) for c in sl_cols), G)
+################### FOR SCALAR COMPARISONS ###################
+# To show R² to 1:1 line  
+for x in [:ER]
+    plot_scalar_correlations_glv(
+        G;
+        scenarios = [x],
+        fit_to_1_1_line=true,
+        save_plot = false,
+        resolution = (1000, 450)
+    )
+end
+
+T = filter(row -> row.scen == :PL, G)
+E = filter(row -> row.scen == :ER, G)
+
+G_low_Beq_cv = filter(row -> row.Beq_cv < 0.6, G)
+G_high_Beq_cv = filter(row -> row.Beq_cv > 0.6, G)
+
+begin
+    want_fit = true
+    colorBy = :conn
+    scen = [:ER]
+    plot_vector_correlations_glv(
+        G;
+        variable=:SL, color_by=colorBy,
+        fit_to_1_1_line=want_fit,
+        scenarios=scen,
+        save_plot = true,
+        resolution = (1000, 450),
+        pixels_per_unit = 6.0
+    )
+end
